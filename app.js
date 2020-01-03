16043369de2811c83489264ce0bd69aa41eaa600
bhoomikag203 <bhoomikag203@gmail.com>
2020-01-03 15:04:24 +0530

    Add | Search movie

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var request = require("request");
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render("search");
+});
+
 app.get('/results', (req, res) => {
-    request("http://www.omdbapi.com/?s=game+of+Thrones&apikey=thewdb", function(error, response, body) {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             var data = JSON.parse(body);
             res.render("results", { data: data });
